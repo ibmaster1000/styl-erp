2883b838ae3b8090c9c09a2d1df3f341ec44a8a3
ibmaster1000 <redeyes9654@gmail.com>
2026-01-05 10:23:23 +0900

    error 2

--- src/main/java/com/example/erp/controller/dto/MaterialTransactionLineView.java
+++ src/main/java/com/example/erp/controller/dto/MaterialTransactionLineView.java
@@ -3,180 +3,256 @@ package com.example.erp.controller.dto;
 import java.math.BigDecimal;
 
 public class MaterialTransactionLineView {
-    private Long bomId;
-    private String stylesId;
-    private String styleCode;
-    private String prdAgreeCode;
-    private String agreementMonth;
-    private String colorCode;
-    private String category;
-    private String materialName;
-    private String materialUsage;
-    private String spec;
-    private String materialColor;
-    private String uom;
-    private BigDecimal qtyPerPiece;
-    private String supplierCode;
-    private String supplierName;
-    private String productionManager;
-    private String orderUom;
-    private BigDecimal unitPrice;
-    private BigDecimal orderQuantity;
-    private BigDecimal inboundQuantity;
-    private BigDecimal outboundQuantity;
-    private String remark;
+	private Long bomId;
+	private String stylesId;
+	private String styleCode;
+	private String prdAgreeCode;
+	private String agreementMonth;
+	private String colorCode;
+	private String category;
+	private String materialName;
+	private String materialUsage;
+	private String spec;
+	private String materialColor;
+	private String uom;
+	private BigDecimal qtyPerPiece;
+	private String supplierCode;
+	private String supplierName;
+	private String productionManager;
+	private String orderUom;
+	private BigDecimal unitPrice;
+	private BigDecimal orderQuantity;
+	private BigDecimal inboundQuantity;
+	private BigDecimal outboundQuantity;
+	private String remark;
 
-    public MaterialTransactionLineView(Long bomId,
-            String stylesId,
-            String styleCode,
-            String prdAgreeCode,
-            String agreementMonth,
-            String colorCode,
-            String category,
-            String materialName,
-            String materialUsage,
-            String spec,
-            String materialColor,
-            String uom,
-            BigDecimal qtyPerPiece,
-            String supplierCode,
-            String supplierName,
-            String productionManager,
-            String orderUom,
-            BigDecimal unitPrice,
-            BigDecimal orderQuantity,
-            BigDecimal inboundQuantity,
-            BigDecimal outboundQuantity,
-            String remark) {
-        this.bomId = bomId;
-        this.stylesId = stylesId;
-        this.styleCode = styleCode;
-        this.prdAgreeCode = prdAgreeCode;
-        this.agreementMonth = agreementMonth;
-        this.colorCode = colorCode;
-        this.category = category;
-        this.materialName = materialName;
-        this.materialUsage = materialUsage;
-        this.spec = spec;
-        this.materialColor = materialColor;
-        this.uom = uom;
-        this.qtyPerPiece = qtyPerPiece;
-        this.supplierCode = supplierCode;
-        this.supplierName = supplierName;
-        this.productionManager = productionManager;
-        this.orderUom = orderUom;
-        this.unitPrice = unitPrice;
-        this.orderQuantity = orderQuantity;
-        this.inboundQuantity = inboundQuantity;
-        this.outboundQuantity = outboundQuantity;
-        this.remark = remark;
-    }
+	public MaterialTransactionLineView() {
+	}
 
-    public MaterialTransactionLineView setSupplierName(String supplierName) {
-        this.supplierName = supplierName;
-        return this;
-    }
+	public MaterialTransactionLineView(Long bomId, String stylesId, String styleCode, String prdAgreeCode,
+			String agreementMonth, String colorCode, String category, String materialName, String materialUsage,
+			String spec, String materialColor, String uom, BigDecimal qtyPerPiece, String supplierCode,
+			String supplierName, String productionManager, String orderUom, BigDecimal unitPrice,
+			BigDecimal orderQuantity, BigDecimal inboundQuantity, BigDecimal outboundQuantity, String remark) {
+		this.bomId = bomId;
+		this.stylesId = stylesId;
+		this.styleCode = styleCode;
+		this.prdAgreeCode = prdAgreeCode;
+		this.agreementMonth = agreementMonth;
+		this.colorCode = colorCode;
+		this.category = category;
+		this.materialName = materialName;
+		this.materialUsage = materialUsage;
+		this.spec = spec;
+		this.materialColor = materialColor;
+		this.uom = uom;
+		this.qtyPerPiece = qtyPerPiece;
+		this.supplierCode = supplierCode;
+		this.supplierName = supplierName;
+		this.productionManager = productionManager;
+		this.orderUom = orderUom;
+		this.unitPrice = unitPrice;
+		this.orderQuantity = orderQuantity;
+		this.inboundQuantity = inboundQuantity;
+		this.outboundQuantity = outboundQuantity;
+		this.remark = remark;
+	}
 
-    public MaterialTransactionLineView setOrderQuantity(BigDecimal orderQuantity) {
-        this.orderQuantity = orderQuantity;
-        return this;
-    }
+	public MaterialTransactionLineView setBomId(Long bomId) {
+		this.bomId = bomId;
+		return this;
+	}
 
-    public MaterialTransactionLineView setInboundQuantity(BigDecimal inboundQuantity) {
-        this.inboundQuantity = inboundQuantity;
-        return this;
-    }
+	public MaterialTransactionLineView setStylesId(String stylesId) {
+		this.stylesId = stylesId;
+		return this;
+	}
 
-    public MaterialTransactionLineView setOutboundQuantity(BigDecimal outboundQuantity) {
-        this.outboundQuantity = outboundQuantity;
-        return this;
-    }
+	public MaterialTransactionLineView setStyleCode(String styleCode) {
+		this.styleCode = styleCode;
+		return this;
+	}
 
-    public Long getBomId() {
-        return bomId;
-    }
+	public MaterialTransactionLineView setPrdAgreeCode(String prdAgreeCode) {
+		this.prdAgreeCode = prdAgreeCode;
+		return this;
+	}
 
-    public String getStylesId() {
-        return stylesId;
-    }
+	public MaterialTransactionLineView setAgreementMonth(String agreementMonth) {
+		this.agreementMonth = agreementMonth;
+		return this;
+	}
 
-    public String getStyleCode() {
-        return styleCode;
-    }
+	public MaterialTransactionLineView setColorCode(String colorCode) {
+		this.colorCode = colorCode;
+		return this;
+	}
 
-    public String getPrdAgreeCode() {
-        return prdAgreeCode;
-    }
+	public MaterialTransactionLineView setCategory(String category) {
+		this.category = category;
+		return this;
+	}
 
-    public String getAgreementMonth() {
-        return agreementMonth;
-    }
+	public MaterialTransactionLineView setMaterialName(String materialName) {
+		this.materialName = materialName;
+		return this;
+	}
 
-    public String getColorCode() {
-        return colorCode;
-    }
+	public MaterialTransactionLineView setMaterialUsage(String materialUsage) {
+		this.materialUsage = materialUsage;
+		return this;
+	}
 
-    public String getCategory() {
-        return category;
-    }
+	public MaterialTransactionLineView setSpec(String spec) {
+		this.spec = spec;
+		return this;
+	}
 
-    public String getMaterialName() {
-        return materialName;
-    }
+	public MaterialTransactionLineView setMaterialColor(String materialColor) {
+		this.materialColor = materialColor;
+		return this;
+	}
 
-    public String getMaterialUsage() {
-        return materialUsage;
-    }
+	public MaterialTransactionLineView setUom(String uom) {
+		this.uom = uom;
+		return this;
+	}
 
-    public String getSpec() {
-        return spec;
-    }
+	public MaterialTransactionLineView setQtyPerPiece(BigDecimal qtyPerPiece) {
+		this.qtyPerPiece = qtyPerPiece;
+		return this;
+	}
 
-    public String getMaterialColor() {
-        return materialColor;
-    }
+	public MaterialTransactionLineView setSupplierCode(String supplierCode) {
+		this.supplierCode = supplierCode;
+		return this;
+	}
 
-    public String getUom() {
-        return uom;
-    }
+	public MaterialTransactionLineView setSupplierName(String supplierName) {
+		this.supplierName = supplierName;
+		return this;
+	}
 
-    public BigDecimal getQtyPerPiece() {
-        return qtyPerPiece;
-    }
+	public MaterialTransactionLineView setProductionManager(String productionManager) {
+		this.productionManager = productionManager;
+		return this;
+	}
 
-    public String getSupplierCode() {
-        return supplierCode;
-    }
+	public MaterialTransactionLineView setOrderUom(String orderUom) {
+		this.orderUom = orderUom;
+		return this;
+	}
 
-    public String getSupplierName() {
-        return supplierName;
-    }
+	public MaterialTransactionLineView setUnitPrice(BigDecimal unitPrice) {
+		this.unitPrice = unitPrice;
+		return this;
+	}
 
-    public String getProductionManager() {
-        return productionManager;
-    }
+	public MaterialTransactionLineView setOrderQuantity(BigDecimal orderQuantity) {
+		this.orderQuantity = orderQuantity;
+		return this;
+	}
 
-    public String getOrderUom() {
-        return orderUom;
-    }
+	public MaterialTransactionLineView setInboundQuantity(BigDecimal inboundQuantity) {
+		this.inboundQuantity = inboundQuantity;
+		return this;
+	}
 
-    public BigDecimal getUnitPrice() {
-        return unitPrice;
-    }
+	public MaterialTransactionLineView setOutboundQuantity(BigDecimal outboundQuantity) {
+		this.outboundQuantity = outboundQuantity;
+		return this;
+	}
 
-    public BigDecimal getOrderQuantity() {
-        return orderQuantity;
-    }
+	public MaterialTransactionLineView setRemark(String remark) {
+		this.remark = remark;
+		return this;
+	}
 
-    public BigDecimal getInboundQuantity() {
-        return inboundQuantity;
-    }
+	public Long getBomId() {
+		return bomId;
+	}
 
-    public BigDecimal getOutboundQuantity() {
-        return outboundQuantity;
-    }
+	public String getStylesId() {
+		return stylesId;
+	}
 
-    public String getRemark() {
-        return remark;
-    }
+	public String getStyleCode() {
+		return styleCode;
+	}
+
+	public String getPrdAgreeCode() {
+		return prdAgreeCode;
+	}
+
+	public String getAgreementMonth() {
+		return agreementMonth;
+	}
+
+	public String getColorCode() {
+		return colorCode;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public String getMaterialName() {
+		return materialName;
+	}
+
+	public String getMaterialUsage() {
+		return materialUsage;
+	}
+
+	public String getSpec() {
+		return spec;
+	}
+
+	public String getMaterialColor() {
+		return materialColor;
+	}
+
+	public String getUom() {
+		return uom;
+	}
+
+	public BigDecimal getQtyPerPiece() {
+		return qtyPerPiece;
+	}
+
+	public String getSupplierCode() {
+		return supplierCode;
+	}
+
+	public String getSupplierName() {
+		return supplierName;
+	}
+
+	public String getProductionManager() {
+		return productionManager;
+	}
+
+	public String getOrderUom() {
+		return orderUom;
+	}
+
+	public BigDecimal getUnitPrice() {
+		return unitPrice;
+	}
+
+	public BigDecimal getOrderQuantity() {
+		return orderQuantity;
+	}
+
+	public BigDecimal getInboundQuantity() {
+		return inboundQuantity;
+	}
+
+	public BigDecimal getOutboundQuantity() {
+		return outboundQuantity;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
 }
